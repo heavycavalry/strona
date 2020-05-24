246bfae62e3adac66120d027e58c0c4663ba2321
heavycavalry <klaudia.diana.paluch@gmail.com>
2020-05-24 15:15:23 +0200

    xdd

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var modalBtn = document.querySelector('.subscribeBtn');
 var modalBg = document.querySelector('.modal-background')
 var modalClose = document.querySelector('.modal-close');
-modalBtn.addEventListener('click', function(){
+modalBtn.addEventListener('click', function () {
     modalBg.classList.add('background-active')
 });
-modalClose.addEventListener('click', function(){
+modalClose.addEventListener('click', function () {
     modalBg.classList.remove('background-active')
 });

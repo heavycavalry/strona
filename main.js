02add6cc75e977e00ea5fc4184ba77bfe1a8d474
wodomierz <miron.ficak@gmail.com>
2020-06-21 23:21:55 +0200

    xdd

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-var modalBtn = document.querySelector('.subscribeBtn');
-var modalBg = document.querySelector('.modal-background')
-var modalClose = document.querySelector('.modal-close');
-modalBtn.addEventListener('click', function () {
-    modalBg.classList.add('background-active')
-});
-modalClose.addEventListener('click', function () {
-    modalBg.classList.remove('background-active')
+document.addEventListener('DOMContentLoaded', function (event) {
+    var modalBtn = document.querySelector('.subscribeBtn');
+    var modalBg = document.querySelector('.modal-background')
+    var modalClose = document.querySelector('.modal-close');
+    modalBtn.addEventListener('click', function () {
+        modalBg.classList.add('background-active')
+    });
+    modalClose.addEventListener('click', function () {
+        modalBg.classList.remove('background-active')
+    });
+
+    const menuIcon = document.querySelector('.hamburger-menu');
+    const navbar = document.querySelector('.mobilenav')
+    menuIcon.addEventListener('click', () => {
+        navbar.classList.toggle('change');
+    });
+
 });
